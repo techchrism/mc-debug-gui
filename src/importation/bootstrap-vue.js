f201ae23a02d3ef5c46c798992e38c40326ee1e7
TheTechdoodle <admin+github@darkender.com>
2020-05-01 11:12:18 -0700

    Update dependencies

--- src/importation/bootstrap-vue.js
+++ src/importation/bootstrap-vue.js
@@ -2,46 +2,44 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import {
-    Button,
-    Card,
-    Layout,
-    Jumbotron,
-    Link,
-    Dropdown,
-    ButtonGroup,
-    FormInput,
-    Spinner,
-    Table,
-    Progress,
-    Collapse,
-    Alert,
-    InputGroup,
-    Pagination,
-    FormGroup,
-    FormCheckbox
-} from 'bootstrap-vue/es/components/';
+    ButtonPlugin,
+    CardPlugin,
+    LayoutPlugin,
+    JumbotronPlugin,
+    LinkPlugin,
+    DropdownPlugin,
+    ButtonGroupPlugin,
+    FormInputPlugin,
+    SpinnerPlugin,
+    TablePlugin,
+    ProgressPlugin,
+    CollapsePlugin,
+    AlertPlugin,
+    InputGroupPlugin,
+    PaginationPlugin,
+    FormGroupPlugin,
+    FormCheckboxPlugin
+} from 'bootstrap-vue';
 
 //import BootstrapVue from 'bootstrap-vue';
 
 export default function importBootstrapVue(Vue)
 {
-    Vue.use(Button);
-    Vue.use(Card);
-    Vue.use(Layout);
-    Vue.use(Jumbotron);
-    Vue.use(Link);
-    Vue.use(Dropdown);
-    Vue.use(ButtonGroup);
-    Vue.use(FormInput);
-    Vue.use(Spinner);
-    Vue.use(Table);
-    Vue.use(Progress);
-    Vue.use(Collapse);
-    Vue.use(Alert);
-    Vue.use(InputGroup);
-    Vue.use(Pagination);
-    Vue.use(FormGroup);
-    Vue.use(FormCheckbox);
-    
-    //Vue.use(BootstrapVue);
+    Vue.use(ButtonPlugin);
+    Vue.use(CardPlugin);
+    Vue.use(LayoutPlugin);
+    Vue.use(JumbotronPlugin);
+    Vue.use(LinkPlugin);
+    Vue.use(DropdownPlugin);
+    Vue.use(ButtonGroupPlugin);
+    Vue.use(FormInputPlugin);
+    Vue.use(SpinnerPlugin);
+    Vue.use(TablePlugin);
+    Vue.use(ProgressPlugin);
+    Vue.use(CollapsePlugin);
+    Vue.use(AlertPlugin);
+    Vue.use(InputGroupPlugin);
+    Vue.use(PaginationPlugin);
+    Vue.use(FormGroupPlugin);
+    Vue.use(FormCheckboxPlugin);
 }
